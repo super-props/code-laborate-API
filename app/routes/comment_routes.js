e71b38f7d07e7b1e1a9f80a85aed6035ad5cc801
MGubernick <MGubernick61@gmail.com>
2021-01-26 15:32:21 -0600

    Add comment routes file
    - mount comment routes in server.js
    
    co-authored-by: Pete V. petevallerie@gmail.com
    co-authored-by: Faith F. faithfitts@gmail.com
    co-authored-by: Nicole S. nicolesurawski@gmail.com

---
+++ app/routes/comment_routes.js
@@ -0,0 +1,23 @@
+const express = require('express')
+// Passport docs: http://www.passportjs.org/docs/
+const passport = require('passport')
+
+// pull in error types and the logic to handle them and set status codes
+const errors = require('../../lib/custom_errors')
+const handle404 = errors.handle404
+const requireOwnership = errors.requireOwnership
+const removeBlanks = require('../../lib/remove_blank_fields')
+
+const Post = require('../models/post')
+
+// passing this as a second argument to `router.<verb>` will make it
+// so that a token MUST be passed for that route to be available
+// it will also set `res.user`
+const requireToken = passport.authenticate('bearer', { session: false })
+
+// instantiate a router (mini app that only handles routes)
+const router = express.Router()
+
+
+
+module.exports = router
