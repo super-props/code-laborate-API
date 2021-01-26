e6b63d4b0a19d90e279b66775b9ff6e9fff9b52e
nicolesurawski <nicolesurawski@gmail.com>
2021-01-26 13:46:10 -0500

    Added Comment and Post Models > > co-authored-by: Matt G. mgubernick61@gmail.com co-authored-by: Pete V. petevallerie@gmail.com co-authored-by: Faith F. faithfitts@gmail.com
    Body in detail

---
+++ app/models/comment.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose')
+
+const commentSchema = new mongoose.Schema({
+  content: {
+    type: String,
+    required: true
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  }
+}, {
+  timestamps: true
+})
+
+module.exports = commentSchema
